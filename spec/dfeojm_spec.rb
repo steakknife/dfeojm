41441d2e06c1f012c58fce10615ee414ad263196
Barry Allard <barry@barryallard.name>
2011-11-19 01:32:57 -0800

    reorganized spec and checked exit codes

--- spec/dfeojm_spec.rb
+++ spec/dfeojm_spec.rb
@@ -2,30 +2,38 @@ require 'dfeojm'
 
 describe DFEOJM do
   it "provides usage information by default" do
-    result = %x[dfeojm 2>&1]
+    output = `dfeojm 2>&1`
+    result = $?.exitstatus
 
-    result.should match /Error: hostname must be present/
-    result.should_not match /is up/
-    result.should_not match /looks down/
+    output.should     match /Error: hostname must be present/
+    output.should_not match /is up/
+    output.should_not match /looks down/
+    result.should_not be    0
   end
 
   it "provides version information" do
-    result = %x[dfeojm -v 2>/dev/null]
+    output = `dfeojm -v 2>/dev/null`
+    result = $?.exitstatus
 
-    result.should match /[0-9]+\.[0-9]+/
+    output.should     match /[0-9]+\.[0-9]+/
+    result.should     be    0
   end
   
   it "shows that www.google.com should usually be up" do
-    result = %x[dfeojm www.google.com 2>/dev/null]
+    output = `dfeojm www.google.com 2>/dev/null`
+    result = $?.exitstatus
 
-    result.should match /is up/
-    result.should_not match /looks down/
+    output.should     match /is up/
+    output.should_not match /looks down/
+    result.should     be    0
   end
 
   it "shows that www.thiswillneverexist.com should usually be down" do
-    result = %x[dfeojm www.thiswillneverexist.com 2>/dev/null]
+    output = `dfeojm www.thiswillneverexist.com 2>/dev/null`
+    result = $?.exitstatus
 
-    result.should match /looks down/
-    result.should_not match /is up/
+    output.should     match /looks down/
+    output.should_not match /is up/
+    result.should_not be    0
   end
 end

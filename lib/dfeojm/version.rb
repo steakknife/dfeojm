76d870b03548eeda82588d418fa48c5eb9018315
Barry Allard <barry.allard@gmail.com>
2013-04-07 19:45:08 -0700

    0.0.3

--- lib/dfeojm/version.rb
+++ lib/dfeojm/version.rb
@@ -1,11 +1,3 @@
 module DFEOJM
-  VERSION = '0.0.2'
-
-  def self.bump_revision
-    found = false
-    File.rewrite(__FILE__).each do |line|
-      # retain the major and minor version, but increment the release.
-      line.sub(/([0-9]*\.[0-9]*\.)([0-9]*).*$/) { found = true ; $1 + "#{$2.to_i+1}" } unless found
-    end
-  end
+  VERSION = '0.0.3'
 end
